3cf445548d86a1d0321c68970dc938d74c71b8d8
SHAIKH ZAEEM ZAHID <zaeem@dcaptech.org>
2025-01-25 14:08:05 +0500

    [200~Carvilla

---
+++ varaible.js
@@ -0,0 +1,15 @@
+
+
+window.addEventListener("scroll",function(){
+
+ const nav=document.getElementById("navbar")
+ if(window.scrollY>50 ){
+
+    nav.style.backgroundColor="black"
+ }
+ else{
+nav.style.backgroundColor="transparent"
+
+ }
+
+})
